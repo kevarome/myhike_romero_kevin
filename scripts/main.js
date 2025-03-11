6ff475ce8d8eb43c41c63bc559065a4f12414f28
Kevin Romero <kromero1@my.bcit.ca>
2025-03-11 11:43:14 -0700

    addded profile page and reviews for each hike - kevin

--- scripts/main.js
+++ scripts/main.js
@@ -95,6 +95,7 @@ function displayCardsDynamically(collection) {
         .then(allHikes=> {
             //var i = 1;  //Optional: if you want to have a unique ID for each hike
             allHikes.forEach(doc => { //iterate thru each doc
+                var docID = doc.id; // get the unique id of the document
                 var title = doc.data().name;       // get value of the "name" key
                 var details = doc.data().details;  // get value of the "details" key
 								var hikeCode = doc.data().code;    //get unique ID to each hike to be used for fetching right image
@@ -106,6 +107,7 @@ function displayCardsDynamically(collection) {
                 newcard.querySelector('.card-length').innerHTML = hikeLength +"km";
                 newcard.querySelector('.card-text').innerHTML = details;
                 newcard.querySelector('.card-image').src = `./images/${hikeCode}.jpg`; //Example: NV01.jpg
+                newcard.querySelector('a').href = "eachHike.html?docID="+docID;
 
                 //Optional: give unique ids to all elements for future use
                 // newcard.querySelector('.card-title').setAttribute("id", "ctitle" + i);
