d6765f95bb058b42ebc41f2930313feb60a699e2
Kevin Romero <kromero1@my.bcit.ca>
2025-04-01 08:55:05 -0700

    Added a jumbrotron on the landing page and customized it - Kevin

--- scripts/main.js
+++ scripts/main.js
@@ -24,7 +24,62 @@
 }*/
 // Function to read the quote of the day from the Firestore "quotes" collection
 // Input param is the String representing the day of the week, aka, the document name
-function readQuote(day) {
+
+
+//Global variable pointing to the current user's Firestore document
+var currentUser;   
+
+//Function that calls everything needed for the main page  
+function doAll() {
+    firebase.auth().onAuthStateChanged(user => {
+        if (user) {
+            currentUser = db.collection("users").doc(user.uid); //global
+            console.log(currentUser);
+
+            // figure out what day of the week it is today
+            const weekday = ["sunday", "monday", "tuesday", "wednesday", "thursday", "friday", "saturday"];
+            const d = new Date();
+            let day = weekday[d.getDay()];
+
+            // the following functions are always called when someone is logged in
+            readQuote(day);
+            insertNameFromFirestore();
+            displayCardsDynamically("hikes");
+        } else {
+            // No user is signed in.
+            console.log("No user is signed in");
+            window.location.href = "login.html";
+        }
+    });
+}
+doAll();
+
+// displays the quote based in input param string "tuesday", "monday", etc. 
+function readQuote( day ) {
+    db.collection( "quotes" ).doc( day ).onSnapshot( doc => {
+        console.log("inside");
+        console.log( doc.data() );
+        document.getElementById( "quote-goes-here" ).innerHTML = doc.data().quote;
+    } )
+}
+// Comment out the next line (we will call this function from doAll())
+// readQuote("tuesday");       
+
+// Insert name function using the global variable "currentUser"
+function insertNameFromFirestore() {
+    currentUser.get().then(userDoc => {
+        //get the user name
+        var user_Name = userDoc.data().name;
+        console.log(user_Name);
+        $("#name-goes-here").text(user_Name); //jquery
+        // document.getElementByID("name-goes-here").innetText=user_Name;
+    })
+}
+// Comment out the next line (we will call this function from doAll())
+// insertNameFromFirestore();
+
+//Old readQuote from premade string
+/*function readQuote(day) {
     db.collection("quotes").doc(day)                                                         //name of the collection and documents should matach excatly with what you have in Firestore
         .onSnapshot(dayDoc => {                                                              //arrow notation
             console.log("current document data: " + dayDoc.data());                          //.data() returns data object
@@ -39,7 +94,7 @@ function readQuote(day) {
             console.log ("Error calling onSnapshot", error);
         });
     }
- readQuote("tuesday");        //calling the function
+ readQuote("tuesday");  */      //calling the function
 
  function writeHikes() {
     //define a variable for the collection you want to create in Firestore to populate data
@@ -91,7 +146,10 @@ function readQuote(day) {
 function displayCardsDynamically(collection) {
     let cardTemplate = document.getElementById("hikeCardTemplate"); // Retrieve the HTML element with the ID "hikeCardTemplate" and store it in the cardTemplate variable. 
 
-    db.collection(collection).get()   //the collection called "hikes"
+    db.collection(collection)
+    .orderBy("length")
+    .limit(3)
+    .get()   //the collection called "hikes"
         .then(allHikes=> {
             //var i = 1;  //Optional: if you want to have a unique ID for each hike
             allHikes.forEach(doc => { //iterate thru each doc
@@ -103,17 +161,40 @@ function displayCardsDynamically(collection) {
                 let newcard = cardTemplate.content.cloneNode(true); // Clone the HTML template to create a new card (newcard) that will be filled with Firestore data.
 
                 //update title and text and image
+                //
+                newcard.querySelector('.card-length').innerHTML =
+                "Length: " + doc.data().length + " km <br>" +
+                "Duration: " + doc.data().hike_time + "min <br>" +
+                "Last updated: " + doc.data().last_updated.toDate().toLocaleDateString();
+                //
                 newcard.querySelector('.card-title').innerHTML = title;
-                newcard.querySelector('.card-length').innerHTML = hikeLength +"km";
+                //newcard.querySelector('.card-length').innerHTML = hikeLength +"km"; //commented out cause the line above also use .cardlength
                 newcard.querySelector('.card-text').innerHTML = details;
                 newcard.querySelector('.card-image').src = `./images/${hikeCode}.jpg`; //Example: NV01.jpg
                 newcard.querySelector('a').href = "eachHike.html?docID="+docID;
+
+                //Th method below only works with one i icon in the html code (the one line bookmark) not multiple. 
+                newcard.querySelector('i').id = 'save-' + docID;   //guaranteed to be unique  This is for the bookmark icon
+                // this line will call a function to save the hikes to the user's document
+                newcard.querySelector('i').onclick = () => saveBookmark(docID);  //EventListener for the bookmark icon
+
+                //Ensure that the bookmark displays correctly as filled if it is already in the favourites
+                //go to the users document and check if the bookmarks array already includes thhe hikes docID
+                //if so display the solid bookmark
+                currentUser.get().then(userDoc => {
+                    //get the user name
+                    var bookmarks = userDoc.data().bookmarks;
+                    if (bookmarks.includes(docID)) {
+                       document.getElementById('save-' + docID).innerText = 'bookmark';
+                    }
+              })
 
                 //Optional: give unique ids to all elements for future use
                 // newcard.querySelector('.card-title').setAttribute("id", "ctitle" + i);
                 // newcard.querySelector('.card-text').setAttribute("id", "ctext" + i);
                 // newcard.querySelector('.card-image').setAttribute("id", "cimage" + i);
 
+                //Once done modifying newcard
                 //attach to gallery, Example: "hikes-go-here"
                 document.getElementById(collection + "-go-here").appendChild(newcard);
 
@@ -123,7 +204,9 @@ function displayCardsDynamically(collection) {
 }
 
 displayCardsDynamically("hikes");  //input param is the name of the collection
-function insertNameFromFirestore() {
+
+//OLD insertName replaced with the one above
+/* function insertNameFromFirestore() {
     // Check if the user is logged in:
     firebase.auth().onAuthStateChanged(user => {
         if (user) {
@@ -141,5 +224,27 @@ function insertNameFromFirestore() {
         }
     })
 }
-insertNameFromFirestore();
-getNameFromAuth(); //run the function
+insertNameFromFirestore(); */
+//getNameFromAuth(); //run the function
+
+//-----------------------------------------------------------------------------
+// This function is called whenever the user clicks on the "bookmark" icon.
+// It adds the hike to the "bookmarks" array
+// Then it will change the bookmark icon from the hollow to the solid version. 
+//-----------------------------------------------------------------------------
+function saveBookmark(hikeDocID) {
+    // Manage the backend process to store the hikeDocID in the database, recording which hike was bookmarked by the user.
+currentUser.update({
+                    // Use 'arrayUnion' to add the new bookmark ID to the 'bookmarks' array.
+            // This method ensures that the ID is added only if it's not already present, preventing duplicates.
+        bookmarks: firebase.firestore.FieldValue.arrayUnion(hikeDocID)
+    })
+            // Handle the front-end update to change the icon, providing visual feedback to the user that it has been clicked.
+    .then(function () {
+        console.log("bookmark has been saved for" + hikeDocID);
+        let iconID = 'save-' + hikeDocID;
+        //console.log(iconID);
+                    //this is to change the icon of the hike that was saved to "filled"
+        document.getElementById(iconID).innerText = 'bookmark';
+    });
+}
